c19bf2bf986e473579386f703c06f1d3faa16d12
Pheinz <pheinz.magnun@gmail.com>
2025-12-28 19:25:14 +0800

    Removed redundant depencies on backend side

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Concerns\ResolvesLandlordAccess;
+use App\Http\Controllers\Permission\ResolvesLandlordAccess;
 use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Property;
